813b8a9ad686598ac13b523361b19e4057a340ef
Serge KAJIE <sergekajie@gmail.com>
2022-12-16 14:40:25 +0100

    updated main

--- main.py
+++ main.py
@@ -9,14 +9,19 @@ logger = config.get_logger("main")
 
 def main():
 
+    logger.info("_____________BEGIN________________")
     logger.info(f"scrapping all reviews")
     reviews_scrapping.get_all_reviews(n_pages=config.n_pages, logger=logger)
+    logger.info("___________________________________________")
 
     logger.info("Processing reviews")
     reviews_processing.reviews_processing(logger=logger)
-    
+    logger.info("___________________________________________")
+
     logger.info("Processing pros and cons")
     pros_and_cons_processing.pros_cons_processing(logger=logger)
+    logger.info("___________________________________________")
+    logger.info("_____________FINISHED________________")
 
 
 if __name__ == "__main__":
